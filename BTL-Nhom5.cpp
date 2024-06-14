a793a18b3070fbacc13fd4b80147a3b1c41e50e1
Vutien2005 <vtien8508@gmail.com>
2024-06-14 23:31:58 +0700

    tien

--- BTL-Nhom5.cpp
+++ BTL-Nhom5.cpp
@@ -1,10 +1,20 @@
 #include<iostream>
+<<<<<<< Updated upstream
 #include<string>
 using namespace std;
 struct Sach{
      string Ten_Sach;
     string Ten_Tac_Gia;
     float Gia_Sach;
+=======
+#include<fstream>
+using namespace std;
+struct Sach
+{
+	string Ten_Sach;
+	string Ten_Tac_Gia;
+	float Gia_Sach;
+>>>>>>> Stashed changes
 };
 typedef Sach keys;
 struct Tree{
@@ -49,6 +59,7 @@ void OutputMinToMax(cay T){// In tu gia thap den gia cao
         OutputMinToMax(T->right);
     }
 }
+<<<<<<< Updated upstream
 void menu(){
     cay T;
     keys x;
@@ -91,4 +102,94 @@ void menu(){
 }
 int main(){
     menu();
+=======
+
+   		// cout<<"10. In sach theo gia tien tu cao den thap\n";
+        // cout<<"11. Doc du lieu tu file\n";
+        // cout<<"10. In danh sach\n";
+void ReadFileToTree(Tree &T){ // case 11;
+	ifstream f1;
+	f1.open("Sach.txt");
+	if(!f1){
+		cout<<"Mo tep loi";
+		return;
+	}
+	Item x;
+	while (!f1.eof())///Đọc đến khi kết thúc file tệp
+	{
+		f1>>x.Ten_Sach;
+		f1.ignore();
+		getline(f1,x.Ten_Sach);
+		f1>>x.Ten_Tac_Gia;
+		f1.ignore();
+		f1>>x.Gia_Sach;
+		AddNode(T,x);
+	}
+	f1.close();
+	cout<<"Da doc xong";
+}
+//Menu
+void Menu(Tree T)
+{
+	int LuaChon;
+	
+	do
+	{
+		cout<<"\nCAC CHUC NANG\n";
+		cout<<"1. Khoi tao\n";
+		cout<<"2. Kiem tra cay rong\n";
+		cout<<"3. Them 1 quyen sach vao danh sach \n";
+        cout<<"4. Tim kiem sach bang ten sach\n";
+        cout<<"5. Tim kiem sach bang ten tac gia\n";
+        cout<<"6. Tim kiem sach theo menh gia\n";
+        cout<<"7. Tim kiem sach theo menh gia cao nhat\n";
+        cout<<"8. Tim kiem sach theo menh gia thap nhat\n";
+        cout<<"9. In sach theo gia tien tu thap den cao\n";
+        cout<<"10. In sach theo gia tien tu cao den thap\n";
+        cout<<"11. Doc du lieu tu file\n";
+        cout<<"12. In danh sach\n";		
+		cout<<"Moi chon chuc nang (1-9)?";
+		cin>>LuaChon;
+		switch(LuaChon)
+		{
+			case 1:// Khởi tạo
+			{
+				Init(T);
+				cout<<"Da khoi tao!";
+				break;
+			}
+			case 2:// Kiểm tra rỗng
+			{
+				if (isEmpty(T))
+					cout<<"Cay rong!";
+				else
+					cout<<"Cay khong trong!";
+				break;
+			}
+			case 3: // Thêm 1 nút vào cây
+			{
+				Item x;
+				cout<<"Nhap thong tin Sach\n";
+				cout<<"Gia Sach: ";
+				cin>>x.Gia_Sach;
+				cout<<"Ten sach: ";
+				getline(cin,x.Ten_Sach);
+				cin.ignore();
+				cout<<"Ho ten Tac Gia: ";
+				getline(cin,x.Ten_Tac_Gia);
+				cin.ignore();
+			
+				AddNode(T,x);
+				cout<<"Da xong!\n";
+				break;
+			}
+			
+			case 11:
+			{
+				ReadFileToTree(T);
+				break;
+			}		
+		}
+	}while(LuaChon!=13);
+>>>>>>> Stashed changes
 }
